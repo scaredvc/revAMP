ede7b262ee5a11ab86ff704131a0a3389cf59788
Hanson <lauhanson318@gmail.com>
2024-12-24 23:37:27 -0800

    added comments to search zones

--- backend/searchZones.py
+++ backend/searchZones.py
@@ -1,5 +1,6 @@
 import cloudscraper
 BASE_URL = "https://aimsmobilepay.com/api/zone/index.php"
+# for searching zones in a frame based on amp backend api
 
 def search_zones(left_long, right_long, top_lat, bottom_lat):
     scraper = cloudscraper.create_scraper()
