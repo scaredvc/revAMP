7d1267360226806240bdf8456fbadfa72145d914
Hanson <lauhanson318@gmail.com>
2025-02-03 23:33:15 -0800

    added context to description function

--- backend/getDescription.py
+++ backend/getDescription.py
@@ -1,4 +1,4 @@
-# function used to get the description of the parking lot.
+# function used to get the description of the parking lot via the api
 def get_description(description_list,location):
     for parkingInfo in location:
         description_list[parkingInfo['description']] = {
