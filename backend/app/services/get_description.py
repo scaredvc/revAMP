a880baaaba3e07d6e260bdf1613bcca1fe34ec95
Hanson <lauhanson318@gmail.com>
2025-08-13 22:31:08 -0700

    Add code field to parking info in get_description function

--- backend/app/services/get_description.py
+++ backend/app/services/get_description.py
@@ -4,6 +4,7 @@ from typing import Dict, Any, List
 def get_description(description_map: Dict[str, Any], locations: List[Dict[str, Any]]) -> None:
     for parking_info in locations:
         description_map[parking_info["description"]] = {
+            "code": parking_info.get("code"),
             "ext_description": parking_info.get("ext_description"),
             "positions": parking_info.get("positions", []),
             "additional_info": parking_info.get("additional_info"),
